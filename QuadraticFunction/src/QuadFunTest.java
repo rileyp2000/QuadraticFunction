5a2d119f026cd039ebbaecaf1812542b51cca4b5
rileyp2000 <rileyp@bishopireton.org>
2017-09-15 09:49:41 -0400

    compare class

---
+++ QuadraticFunction/src/QuadFunTest.java
@@ -0,0 +1,15 @@
+/**
+ * <p>this is a comaparator class for the quadratic functions</p> 
+ * Sep 15, 2017
+ * @author Patrick Riley
+ */
+
+import java.util.Comparator;
+
+/**
+ * @author rileyp
+ *
+ */
+public class QuadFunTest {
+
+}
