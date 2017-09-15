b346a05e028a32f2efc5295d95c63a7c04a91a6d
rileyp2000 <rileyp@bishopireton.org>
2017-09-15 09:48:10 -0400

    made compareTo

--- QuadraticFunction/src/QuadraticFunction.java
+++ QuadraticFunction/src/QuadraticFunction.java
@@ -8,18 +8,17 @@
  * @author rileyp
  *
  */
-public class QuadraticFunction implements Comparable<QuadraticFunction>{
-	private int a,b,c;
-	
-	
+public class QuadraticFunction implements Comparable<QuadraticFunction> {
+	private int a, b, c;
+
 	public QuadraticFunction(int aa, int bb, int cc) {
 		a = aa;
 		b = bb;
 		c = cc;
 	}
-	
-	//GETTERS AND SETTERS
-	
+
+	// GETTERS AND SETTERS
+
 	/**
 	 * @return the a
 	 */
@@ -28,13 +27,13 @@ public class QuadraticFunction implements Comparable<QuadraticFunction>{
 	}
 
 	/**
-	 * @param a the a to set
+	 * @param a
+	 *            the a to set
 	 */
 	public void setA(int a) {
 		this.a = a;
 	}
 
-	
 	/**
 	 * @return the b
 	 */
@@ -43,13 +42,13 @@ public class QuadraticFunction implements Comparable<QuadraticFunction>{
 	}
 
 	/**
-	 * @param b the b to set
+	 * @param b
+	 *            the b to set
 	 */
 	public void setB(int b) {
 		this.b = b;
 	}
 
-	
 	/**
 	 * @return the c
 	 */
@@ -58,14 +57,15 @@ public class QuadraticFunction implements Comparable<QuadraticFunction>{
 	}
 
 	/**
-	 * @param c the c to set
+	 * @param c
+	 *            the c to set
 	 */
 	public void setC(int c) {
 		this.c = c;
 	}
-	
-	//<METHODS
-	
+
+	// <METHODS
+
 	/**
 	 * 
 	 * @param x
@@ -73,51 +73,46 @@ public class QuadraticFunction implements Comparable<QuadraticFunction>{
 	 *
 	 */
 	public double valueAt(double x) {
-		return a*Math.pow(x,2) + b*x + c;
+		return a * Math.pow(x, 2) + b * x + c;
 	}
-	
+
 	/**
-	 *  @param other the Other object
-	 *  @return if equal coefficients
+	 * @param other
+	 *            the Other object
+	 * @return if equal coefficients
 	 */
-	public boolean equals(Object other){
-		  if(a == ((QuadraticFunction)other).getA() && 
-				  b == ((QuadraticFunction)other).getB() && 
-		              c == ((QuadraticFunction)other).getC() )
-		     return true;
-		  else
-			 return false;
-		   
+	public boolean equals(Object other) {
+		if (a == ((QuadraticFunction) other).getA() && b == ((QuadraticFunction) other).getB()
+				&& c == ((QuadraticFunction) other).getC())
+			return true;
+		else
+			return false;
+
 	}
-	
-	
+
 	/**
 	 * 
 	 * @param other
-	 * @return int 
+	 * @return int
 	 *
 	 */
 	public int compareTo(QuadraticFunction other) {
-		if(a - other.getA() != 0)
+		if (a - other.getA() != 0)
 			return a - other.getA();
+		else if (b - other.getB() != 0)
+			return a - other.getB();
 		else
-			if(b - other.getB() != 0)
-				return a - other.getB();
-			else
-					return c - other.getC();
+			return c - other.getC();
 	}
-	
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return a + "x^2" + "+" + b + "x" + c;
 	}
-	
-	
-	
-	
+
 }
